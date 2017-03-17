d5a0c1def52d5c73f8c7b3bfeb39c37ff79802c8
root <root@vultr.guest>
2017-03-18 00:22:24 +0800

    first try

---
+++ notepad.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+  <style>
+    #Wapper {
+             margin:10px 5px 10px 5px;
+	    }
+
+    #Main {
+	     margin:50px 50px 20px 50px;
+	     padding: 50px 300px 50px 300px;
+	     background-color:gray;
+	     color:white;
+	    }
+  </style>
+</head>
+<body>
+  <div id="Wapper">
+    <div id="Main">
+      <p><center>Hello!</center></p>
+      <p>Left align</p>
+    </div>
+  </div>
+</body>
